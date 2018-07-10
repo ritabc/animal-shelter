822521679bfef20587e8901111e072c5fb05d385
Nate Cottle and Rita Bennett-Chew <nate@>
2018-07-10 12:02:12 -0700

    Pass 11 tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -31,4 +31,17 @@ class Customer
     self.name.==(another_customer.name).&(self.phone.==(another_customer.phone)).&(self.species_pref.==(another_customer.species_pref)).&(self.breed_pref.==(another_customer.breed_pref))
   end
 
+  def self.list_by_breed(breed)
+    list_of_customers = DB.exec("SELECT * FROM customers WHERE breed_pref = '#{breed}';")
+    customers = []
+    list_of_customers.each do |customer|
+      id = customer.fetch("id").to_i
+      name = customer.fetch("name")
+      phone = customer.fetch("phone")
+      species_pref = customer.fetch("species_pref")
+      breed_pref = customer.fetch("breed_pref")
+      customers.push(Customer.new({:id => id, :name => name, :phone => phone, :species_pref => species_pref, :breed_pref => breed_pref}))
+    end
+    customers
+  end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -30,4 +30,16 @@ describe('Customer') do
       expect(customer_test).to(eq(customer_test2))
     end
   end
+
+  describe('#list_by_breed') do
+    it('will list all the customers and phones by breed preference') do
+      customer_test = Customer.new({:id => nil, :name => 'Tedd', :phone => '919-555-1234', :species_pref => 'Dog', :breed_pref => 'Shitzu'})
+      customer_test2 = Customer.new({:id => nil, :name => 'Fredd', :phone => '919-555-1234', :species_pref => 'Dog', :breed_pref => 'Labrador'})
+      customer_test3 = Customer.new({:id => nil, :name => 'Ned', :phone => '919-555-1234', :species_pref => 'Dog', :breed_pref => 'Shitzu'})
+      customer_test.save()
+      customer_test2.save()
+      customer_test3.save()
+      expect(Customer.list_by_breed('Shitzu')).to(eq([customer_test, customer_test3]))
+    end
+  end
 end
